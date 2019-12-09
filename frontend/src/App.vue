9b865d8d719bc8c223798ecdb9cab0044ae684c4
Linnea <linneaj95@hotmail.com>
2019-12-09 14:40:22 +0100

    Created pages, changed routers

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Start</router-link> |
+      <router-link to="/soker-hem">Söker hem</router-link> |
+      <router-link to="/hittat-hem">Hittat hem</router-link> |
+      <router-link to="/information">Senare problem</router-link> |
+      <router-link to="/kontakt">Kontakt</router-link> |
+      <router-link to="/admin">Admin</router-link> |
+      <router-link to="/logga-in">Logga in</router-link>
     </div>
     <router-view/>
   </div>
