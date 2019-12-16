932f753fc22d02059de077e5de188d92ae4a8211
Linnea <linneaj95@hotmail.com>
2019-12-16 15:34:51 +0100

    Signup-funciton and login-function

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,20 +6,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    animals: []
+    animals: [],
+    loggedInUser: ""
   },
   mutations: {
     setAnimals(state, animals) {
       state.animals = animals;
+    },
+    setLoggedInUser(state, user) {
+      state.loggedInUser = user;
     }
   },
   actions: {
+    // animals
     async getAllAnimals(ctx){
       let animals = await axios.get('http://localhost:3000/animals')
       await ctx.commit('setAnimals', animals.data);
     },
     async createAnimal(ctx, animal) {
       await axios.post('http://localhost:3000/animals', animal);
+    },
+    // users
+    async signUp(ctx, userInfo) {
+      await axios.post('http://localhost:3000/users', userInfo);
+    },
+    async login(ctx, userInfo) {
+      try {
+        let verify = await axios.post('http://localhost:3000/verify', userInfo);
+        ctx.commit('setLoggedInUser', verify.data.username)
+      }
+      catch (err) {
+        alert(err)
+      }
     }
   },
   modules: {
@@ -27,6 +45,9 @@ export default new Vuex.Store({
   getters: {
     getAnimals(state){
       return state.animals;
+    },
+    getLoggedInUser(state){
+      return state.loggedInUser;
     }
   }
 })
