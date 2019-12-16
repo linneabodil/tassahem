b80a66ffd67be2399dd161c554567b47c887aa42
Vendela <vendelapatricia@hotmail.com>
2019-12-16 13:17:30 +0100

    Arranged scss to folders and fixed burger-menu

--- frontend/src/components/TextBox.vue
+++ frontend/src/components/TextBox.vue
@@ -24,41 +24,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-$petrol: #4B8E8D;
-$pink: #C3377E;
-$orange: #F59039; 
 
-*{
-  color: white;
-}
-
-  .box{
-    background: $petrol;
-    text-align: center;
-    padding: 2rem;
-    
-    h1{
-      font-family: Bebas Neue;
-      font-size: 36px;
-    }
-
-    h2{
-      font-family: Bebas Neue;
-      font-size: 24px;
-    }
-
-    p{
-      font-family: Noto Serif;
-    }
-
-    button{
-      background: black;
-      width: 11rem;
-      margin-top: 2rem;
-
-      h2{
-        margin: .5rem;
-      }
-    }
-  }
 </style>
