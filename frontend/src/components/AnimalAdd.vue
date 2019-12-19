e5d6443855c0a78621610fb6aea41eda7d7334a3
reeklof <reeklof@live.com>
2019-12-19 12:03:26 +0100

    styling animal cards

--- frontend/src/components/AnimalAdd.vue
+++ frontend/src/components/AnimalAdd.vue
@@ -1,88 +1,106 @@
 <template>
   <div class="addAnimal">
-    <h1>Lägg in djur</h1>
-    <label>Namn:</label>
-    <input type="text" placeholder="Namn" v-model="newAnimal.name">
-    <label>Födelseår:</label>
-    <input type="date" placeholder="xxxx-xx-xx" v-model="newAnimal.born">
-    <label>Ras:</label>
-    <input type="text" placeholder="Ras" v-model="newAnimal.breed">
-    <br>
-    <label>Typ:</label>
-    <input type="radio" id="cat" value="cat" v-model="newAnimal.category">
-    <label for="female">Katt</label>
-    <input type="radio" id="dag" value="dog" v-model="newAnimal.category">
-    <label for="male">Hund</label>
-    <br>
-    <label>Kön:</label>
-    <input type="radio" id="female" value="female" v-model="newAnimal.gender">
-    <label for="female">Tjej</label>
-    <input type="radio" id="male" value="male" v-model="newAnimal.gender">
-    <label for="male">Kille</label>
-    <br>
-    <label>Beskrivning:</label>
-    <textarea type="text" placeholder="Beskrivning" v-model="newAnimal.description"></textarea>
-    <label>Behov:</label>
-    <select v-model="newAnimal.needs" multiple>
-      <option>Utevistelse</option>
-      <option>Hem utan barn</option>
-      <option>Kattkompis</option>
-      <option>Hundkompis</option>
-    </select>
-    <label>Status:</label>
-    <select v-model="newAnimal.status">
-      <option value="seeking">Söker hem</option>
-      <option value="found">Hittat hem</option>
-    </select>
-    <label>img</label>
-    <input type="text" placeholder="img" v-model="newAnimal.img">
     <section>
-      <a href="#" class="btn-add" @click="createAnimal">Spara</a>
-      <a href="#" class="btn-cancel" @click="cancelNewAnimal">Avbryt</a>
+      <img class="__photo" alt="Bild på djur" src="../assets/img/dogwalker.jpg">
+    </section>
+    <div class="__container">
+      <h2>Skapa djur</h2>
+      <section>
+        <label>Namn:</label>
+        <input type="text" placeholder="Djurets namn" v-model="newAnimal.name">
+      </section>
+      <section>
+        <label>Födelseår:</label>
+        <input type="text" placeholder="xxxx-xx-xx" v-model="newAnimal.born">
+      </section>
+      <section>
+        <label>Ras:</label>
+        <input type="text" placeholder="Ras" v-model="newAnimal.breed">
+      </section>
+      <section class="__radio">
+        <label>Typ:</label>
+        <input type="radio" id="cat" value="cat" v-model="newAnimal.category">
+        <label for="female">Katt</label>
+        <input type="radio" id="dag" value="dog" v-model="newAnimal.category">
+        <label for="male">Hund</label>
+      </section>
+      <section class="__radio">
+        <label>Kön:</label>
+        <input type="radio" id="female" value="female" v-model="newAnimal.gender">
+        <label for="female">Tjej</label>
+        <input type="radio" id="male" value="male" v-model="newAnimal.gender">
+        <label for="male">Kille</label>
+      </section>
+      <section class="__input">
+        <label>Beskrivning:</label>
+        <textarea placeholder="Beskriv djurets personlighet..." v-model="newAnimal.description"/>
+      </section>
+      <section class="__input">
+        <label>Behov:</label>
+        <select v-model="newAnimal.needs" multiple>
+          <option>Utevistelse</option>
+          <option>Hem utan barn</option>
+          <option>Kattkompis</option>
+          <option>Hundkompis</option>
+        </select>
+      </section>
+      <div class="__change">
+        <h3>Status:</h3>
+        <select v-model="newAnimal.status">
+          <option value="seeking">Söker hem</option>
+          <option value="found">Hittat hem</option>
+        </select>
+      </div>
+      <section>
+        <label>Bild:</label>
+        <input type="text" placeholder="bildlänk..." v-model="newAnimal.img">
+      </section>
+    </div>
+    <section class="__btn-action">
+      <a href="#" class="btn btn-animal" @click="createAnimal">Spara</a>
+      <a href="#" class="btn btn-animal" @click="cancelNewAnimal">Avbryt</a>
     </section>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'AnimalAdd',
-  data () {
+  name: "AnimalAdd",
+  data() {
     return {
       newAnimal: {
-        name: '',
-        born: '',
-        breed: '',
-        category: '',
-        gender: '',
-        description: '',
+        name: "",
+        born: "",
+        breed: "",
+        category: "",
+        gender: "",
+        description: "",
         needs: [],
-        status: '',
-        img: ''
+        status: "",
+        img: ""
       }
-    }
+    };
   },
   methods: {
-    async createAnimal(){
-      await this.$store.dispatch('createAnimal', this.newAnimal)
-      await this.$store.dispatch('getAllAnimals');
+    async createAnimal() {
+      await this.$store.dispatch("createAnimal", this.newAnimal);
+      await this.$store.dispatch("getAllAnimals");
       this.clearInput();
     },
-    clearInput(){
-      this.newAnimal.name ='',
-      this.newAnimal.born ='',
-      this.newAnimal.breed ='',
-      this.newAnimal.category ='',
-      this.newAnimal.gender ='',
-      this.newAnimal.description =''
-      this.newAnimal.needs =[]
-      this.newAnimal.status =''
-      this.newAnimal.img = ''
+    clearInput() {
+      this.newAnimal.name = "";
+      this.newAnimal.born = "";
+      this.newAnimal.breed = "";
+      this.newAnimal.category = "";
+      this.newAnimal.gender = "";
+      this.newAnimal.description = "";
+      this.newAnimal.needs = [];
+      this.newAnimal.status = "";
+      this.newAnimal.img = "";
     },
     cancelNewAnimal() {
       this.clearInput();
     }
   }
-}
-
+};
 </script>
--- frontend/src/components/AnimalCard.vue
+++ frontend/src/components/AnimalCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <aside class="card">
+  <aside class="animalcard">
     <img class="__photo" :src="animal.img" alt="Bild på djur">
     <div class="__section">
       <h2 :class="animal.category">
         {{ animal.name }}
-        <img :src="require('../assets/' + animal.category + '.svg')" alt>
-        <img :src="require('../assets/' + animal.gender + '.svg')" alt>
+        <img :src="require('../assets/' + animal.category + '-black.svg')" alt>
+        <img :src="require('../assets/' + animal.gender + '-black.svg')" alt>
       </h2>
       <div class="animal__text" :class="animal.category"></div>
       <p>Född {{ animal.born.slice(0,10) }}</p>
@@ -15,7 +15,9 @@
         <p v-for="(need, index) in animal.needs" :key="index">{{need}}</p>
       </div>
     </div>
-    <p @click="$router.push(`/AnimalEdit/${animal._id}`)">Redigera</p>
+    <div class="__btn-action">
+      <p class="btn btn-animal" @click="$router.push(`/AnimalEdit/${animal._id}`)">Redigera</p>
+    </div>
   </aside>
 </template>
 
